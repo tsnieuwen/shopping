8a6c573e9d73e3af4e61baac0ab8980383ed0498
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-20 09:09:31 -0700

    Readable item attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,10 @@
 class Item
+  attr_reader :name,
+              :price
 
   def initialize(data)
-  end 
+    @name = data[:name]
+    @price = (data[:price].gsub("$", "")).to_f
+  end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -9,4 +9,11 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, item1
   end
 
+  def test_it_has_readable_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_equal "Tomato", item2.name
+    assert_equal 0.5, item2.price
+  end
+
 end
